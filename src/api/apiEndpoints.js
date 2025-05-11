71559aeaa08bc19f346cf5b18e24435ef24665fc
R Dinesh Kumar Reddy <163737305+2300031102dinesh@users.noreply.github.com>
2025-05-11 07:10:16 +0530

    Update apiEndpoints.js

--- src/api/apiEndpoints.js
+++ src/api/apiEndpoints.js
@@ -1,5 +1,7 @@
 // Define your base URL here
-const BASE_URL = "http://localhost:5000/api/v1";
+// const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://backend-expense-f632.onrender.com";
+
 
 // Auth endpoints
 export const AUTH_API = {
